07497ee01044de87825458a85523e82b6b3248de
sejarparvez <sejarparvez@yahoo.com>
2024-10-20 12:25:14 +0600

    removed swiper

--- src/components/pages/about/testimonials/Testimonials.tsx
+++ src/components/pages/about/testimonials/Testimonials.tsx
@@ -1,18 +1,17 @@
 "use client";
-
-import { useRef } from "react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { TestimonialArrowButton } from "./TestimonialArrowButton";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 import { TestimonialCard } from "./TestimonialCard";
 import { TestimonialContent } from "./TestimonialContent";
 import { TestimonialData } from "./TestimonialsData";
 
 export default function Testimonials() {
-  const swiperRef = useRef<any>(null);
-
   return (
     <div className="mx-3 my-16 md:mx-10 lg:mx-auto lg:max-w-7xl">
       <div className="text-center">
@@ -20,39 +19,34 @@ export default function Testimonials() {
       </div>
 
       <div className="relative lg:mx-20">
-        {/* Custom previous button */}
-        <TestimonialArrowButton
-          direction="left"
-          onClick={() => swiperRef.current?.slidePrev()}
-        />
-
-        <Swiper
-          grabCursor
-          loop
-          pagination
-          navigation
-          modules={[Pagination]}
-          onSwiper={(swiper) => {
-            swiperRef.current = swiper;
+        <Carousel
+          className="w-full"
+          opts={{
+            loop: true,
           }}
-          spaceBetween={50}
-          slidesPerView={1}
+          plugins={[
+            Autoplay({
+              delay: 4000,
+            }),
+          ]}
         >
-          {TestimonialData.map((review) => (
-            <SwiperSlide key={review.id}>
-              <div className="mt-20 flex flex-col justify-center md:flex-row md:gap-10">
-                <TestimonialCard review={review} />
-                <TestimonialContent review={review} />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        {/* Custom next button */}
-        <TestimonialArrowButton
-          direction="right"
-          onClick={() => swiperRef.current?.slideNext()}
-        />
+          <CarouselContent className="relative">
+            {TestimonialData.map((review) => (
+              <CarouselItem key={review.id}>
+                <div className="mt-20 flex flex-col justify-center md:flex-row md:gap-10">
+                  <TestimonialCard review={review} />
+                  <TestimonialContent review={review} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="absolute right-40 top-20 scale-150">
+            <CarouselPrevious />
+          </div>
+          <div className="absolute right-10 top-20 scale-150">
+            <CarouselNext />
+          </div>
+        </Carousel>
       </div>
     </div>
   );
